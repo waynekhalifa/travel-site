3c140583f17f046bd0951d783c6b8616c57e04ce
Wayne Khalifa <wani@start-solutions.net>
2018-08-12 14:53:49 +0200

    completed stiky header feature

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,7 +1,9 @@
 import $ from 'jquery';
 import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/RevealOnScroll';
+import StikyHeader from './modules/StikyHeader';
 
 let menu = new MobileMenu();
 new RevealOnScroll( $('.feature-item'), '85%' );
 new RevealOnScroll( $('.testimonial'), '60%' );
+let stikyHeader = new StikyHeader();
---
+++ app/assets/scripts/modules/StikyHeader.js
@@ -0,0 +1,65 @@
+import $ from 'jquery';
+import waypoints from '../../../../node_modules/waypoints/lib/noframework.waypoints';
+import smoothScroll from 'jquery-smooth-scroll';
+
+class StikyHeader {
+	constructor() {
+		this.siteHeader = $('.site-header');
+		this.headerTrigger = $('.large-hero__title');
+		this.createHeaderWaypoint();
+		this.pageSection = $('.page-section');
+		this.createPageSectionWaypoints();
+		this.headerLinks = $('.navigation a');
+		this.addSmoothScrolling();
+	}
+
+	addSmoothScrolling() {
+		this.headerLinks.smoothScroll();
+	}
+
+	createHeaderWaypoint() {
+		let that = this;
+		new Waypoint({
+			element: this.headerTrigger[0],
+			handler: function( direction ) {
+				if ( direction == 'down' ) {
+					that.siteHeader.addClass('site-header--dark');
+				} else {
+					that.siteHeader.removeClass('site-header--dark');
+				}
+			}
+		});
+	}
+
+	createPageSectionWaypoints() {
+		let that = this;
+		this.pageSection.each(function() {
+			let currentPageSection = this;
+
+			new Waypoint({
+				element: currentPageSection,
+				handler: function( direction ) {
+					if ( direction == 'down') {
+						let matchingHeaderLink = currentPageSection.getAttribute('data-matching-link');
+						that.headerLinks.removeClass('is-current-link');
+						$(matchingHeaderLink).addClass('is-current-link');
+					}
+				},
+				offset: '18%'
+			});
+
+			new Waypoint({
+				element: currentPageSection,
+				handler: function( direction ) {
+					if ( direction == 'up') {
+						let matchingHeaderLink = currentPageSection.getAttribute('data-matching-link');
+						that.headerLinks.removeClass('is-current-link');
+						$(matchingHeaderLink).addClass('is-current-link');
+					}
+				},
+				offset: '-40%'
+			});
+		});
+	}
+}
+export default StikyHeader;
